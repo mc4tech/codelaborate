c799ce31a08ce4d1bd321337bf8a7bd0d1dc8938
gtrmonkey <gtrmonkey@gmail.com>
2018-11-29 21:13:13 -0500

    Test username

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
 // Write a positive unit test
+describe('Unqinename', function() {
+    beforeEach(function(){
+        const users =["alex", "matt","gabe"]
+    })
+it('should enforce unqine username', function() {
+    expect(unqinename("fakename")).to.equal(true);
+  });
+it('should each user an unqine I.D'),function(){
 
-
+}
 // Write a negative unit test
+it('should enforce unqine username', function() {
+    expect(unqinename("matt")).to.equal(false);
+  });
+});
