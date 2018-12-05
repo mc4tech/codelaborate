232b43196ea790ceba035b1e1f4a247d494b32db
gtrmonkey <gtrmonkey@gmail.com>
2018-12-04 19:07:01 -0500

    projectschemadone

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,16 @@
 const db = require('../models');
 module.exports = function(app) {
+  app.post('/api/users'), function (req, res) {
+    console.log('------Adding Link in mongo');
+    db.user.create(req.body)
+        .then(function (dbusers) {
+            res.json(dbusers);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
 
+  }
   app.get('/api/users', function(req, res) {
     db.User.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
