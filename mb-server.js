df74146e2052cf4905a39629cc89d01a6404396c
Matt Byrnes <mattjbyrnes@gmail.com>
2018-12-06 18:55:13 -0500

    Commit CodeMirror 3

---
+++ mb-server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+// const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/todoio', { useNewUrlParser: true });
+// mongoose.connect('mongodb://bcmatt:bcpass1@ds163176.mlab.com:63176/heroku_fjn2c96d', { useNewUrlParser: true });
+
+// require('./sockets/todo-sockets.js')(io);
+// require('./routes/html-routes.js')(app);
+// require('./routes/api-routes.js')(app);
+
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
+
+
+server.listen(PORT, function () {
+	console.log(`Server is listening on port ${PORT}`)
+})
+
+
--- server.js
+++ server.js
@@ -1,58 +1,13 @@
-const express = require("express");
-// const mongoose = require("mongoose");    
-// const passport  = require("passport");    
-
-//////////////////////
-//LocalStrategy = require("passport-local");   passportLocalMongoose = require("passport-local-mongoose");
-/////////////////////
-
-const PORT = process.env.PORT || 5000;
-const app = express();
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-///
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-
-
-// Mongoose connection ** HAVE TO EDIT URI ***
-// mongoose.Promise = global.Promise;
-
-// mongoose.connect(
-  
-//   process.env.MONGODB_URI ||  "mongodb://user:pandapass1@ds259820.mlab.com:59820/heroku_d3j0q25z", {
-
-//   useMongoClient: true
-//   }
-//  );
-
-
-// require('./routes/api-routes.js')(app);
-require('./routes/html-routes.js')(app);
-
-
-app.listen(PORT, function() {
-  console.log(`App running on port ${PORT}`);
-});
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// Set the port to 5000 OR let the process set the port (if deployed to Heroku)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-// Initialize Express
 const app = express();
 
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Use express.static to serve the public folder as a static directory
 app.use(express.static('public'));
 
 // Connect to the Mongo DB using the codelaborate database (will be created if it doesn't exist)
